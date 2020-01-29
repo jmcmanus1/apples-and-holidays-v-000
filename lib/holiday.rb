fc9eea18ce231e20f9e6208da8955548a9c92a52
Julie  McManus <julieannesays@gmail.com>
2020-01-29 23:57:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,60 +1,59 @@
-require 'pry'
+require "pry"
 
-def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_hash[:summer][:fourth_of_july][1]
+holiday_supplies_hash = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}
+
+def second_supply_for_fourth_of_july(holiday_supplies_hash)
+  holiday_supplies_hash[:spring][:memorial_day].join("")
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter][:christmas] = supply
-  holiday_hash[:winter][:new_years] = supply
-  
-end
- 
-
-def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:spring][:memorial_day] = supply
-end
-
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season] = {holiday_name => supply_array}
-  holiday_hash   
-end
-
-def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].values.to_s
-end
-
-def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, data|
-    puts "#{season.to_s.capitalize!}:"
-      data.each do |holiday, supply|
-      stringed = holiday.to_s
-      remove_underscore = stringed.gsub("_", " ")
-      capitalized = remove_underscore.split.each {|i| i.capitalize!}.join(" ")
-      holiday = capitalized
-      puts "  #{holiday}: #{supply.join(", ")}"
-     end
-   end
+def add_supply_to_winter_holidays(holiday_supplies_hash, data)
+  holiday_supplies_hash.each do |season, holidays|
+    if season == :winter
+      holidays.collect do |key, value|
+      value << ("Balloons")
+      end
+    end
   end
+end
 
-def all_holidays_with_bbq(holiday_hash)
-  selected_holiday = []
-    holiday_hash.each do |season, data|
-      data.each do |holiday, supply|
-       supply.each do |item|
-        if item == "BBQ"
-           selected_holiday << holiday
-         end
-       end
-     end
-   end
-   selected_holiday
-  end     
+def add_supply_to_memorial_day(holiday_supplies_hash, data)
+  holiday_supplies_hash.each do |season, holidays|
+    if season == :spring
+      holidays.collect do |key, value|
+      value << ("Grill") << ("Table Cloth")
+      end
+    end
+  end
+end
 
+def add_new_holiday_with_supplies(holiday_supplies_hash, season, holiday, supplies)
+  holiday_supplies_hash[season][holiday] = supplies
+end
 
+def all_winter_holiday_supplies(holiday_supplies_hash)
+  holiday_supplies_hash[:winter].values.flatten
+end
 
-
-
-
-
-
+def all_supplies_in_holidays(holiday_supplies_hash)
+  holiday_supplies_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+    puts "#{season.to_s.capitalize!}:"
+    binding.pry
+    puts "  #{holiday.to_s.tr("_"," ").}: #{supplies.join(", ")}"
+    end
+  end
+end
