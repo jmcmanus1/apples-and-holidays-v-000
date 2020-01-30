8ba280545202b3aab967a0e288c41562ebb48028
Julie  McManus <julieannesays@gmail.com>
2020-01-30 00:49:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def all_supplies_in_holidays(holiday_supplies_hash)
       puts "#{season.to_s.capitalize!}:"
     holidays.each do |holiday, supplies|
       holiday = holiday.to_s.tr("_"," ").split.each {|word| word.capitalize!}.join(" ")
-    puts "  #{holiday.join(" ")}: #{supplies.join(", ")}"
+    puts "  #{holiday}: #{supplies.join(", ")}"
     end
   end
 end
