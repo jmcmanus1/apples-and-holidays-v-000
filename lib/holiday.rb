261aa82e7ffbb4937ca9b2324b4aecd262a10499
Julie  McManus <julieannesays@gmail.com>
2020-01-30 01:16:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,5 +48,8 @@ def all_supplies_in_holidays(holiday_supplies_hash)
 end
 
 def all_holidays_with_bbq(holiday_supplies_hash)
+  holiday_supplies_hash.each do |season, data|
+    data.select {|holiday, supplies| supplies == "BBQ"}
+
 
 end
